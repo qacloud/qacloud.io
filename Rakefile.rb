ac7ffbd26b1d532ac54db0f5d4ee36ba8007333c
qacloud <qacloud.io@gmail.com>
2014-11-05 17:08:43 -0800

    Update Rakefile.rb
    updated file

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,9 +2,14 @@
 # task :default => [:test]
 
 # task :test1 do
-  ruby "playlist.rb 542456b0acb0470200000010"
+#  ruby "playlist.rb 542456b0acb0470200000010"
 # end
 
 # task :test2 do
 #   ruby "playlist.rb 5455c475650ac50200000002"
 # end
+
+task :playlistCI do
+  plid = ENV["playlistid"]
+  ruby "plist.rb #{plid}"
+end
