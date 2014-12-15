3f8b41057f5f507f450c176702513ad87bb359a4
qacloud.io <qacloud.io@gmail.com>
2014-12-15 14:24:19 -0800

    change location of playlistID var

--- playlist.rb
+++ playlist.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 require 'qacloud'
-puts playlistID
+
 playlistID = ARGV[0]
+puts playlistID
 # tokenID = "oChbNdoggDrVhIv/dL5cXkkUMflCASw3K/R2tUyyVa3HW1njpNOv3sXRElPxaj58"
 tokenID = "dW8MbiB1o7ezufY9xJNtRvTe5iISaoqotcS/AOl+thyeNFje+RglWZRuLA7FFzTi"
 # tokenID = "0/Hxy85jkc9ZgSWAmwPFE/kLD60MBwwI6gDmGTHbGNBqt9sOehujaoZA62zccR0r"
